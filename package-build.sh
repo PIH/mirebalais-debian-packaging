64c61efca8132dc608c27ae80bda0bf10c456d04
mogoodrich <mgoodrich@pih.org>
2015-07-01 09:33:56 -0400

    update debian package for tomcat7

--- package-build.sh
+++ package-build.sh
@@ -4,8 +4,8 @@ ARTIFACTS=/opt/bamboo-home/artifacts
 OMOD_ARTIFACT=$ARTIFACTS/MIREBALAIS-INTEGRATION/shared
 LATEST_OMOD=`ls -t $OMOD_ARTIFACT/|head -1`
 
-mkdir home/tomcat6/.OpenMRS/modules
-unzip -j $OMOD_ARTIFACT/$LATEST_OMOD/omod-zip/mirebalais-distribution*.zip -d  home/tomcat6/.OpenMRS/modules/
+mkdir home/tomcat7/.OpenMRS/modules
+unzip -j $OMOD_ARTIFACT/$LATEST_OMOD/omod-zip/mirebalais-distribution*.zip -d  home/tomcat7/.OpenMRS/modules/
 
 WAR_ARTIFACT=$ARTIFACTS/MIREBALAIS-OPENMRS/shared
 LATEST_WAR=`ls -t $WAR_ARTIFACT/|head -1`
