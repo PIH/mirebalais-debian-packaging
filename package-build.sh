1ea47d582c91d92b4fcea52d8ee6bf6c8ff6eb1a
Brandon Istenes <bistenes@gmail.com>
2021-12-17 12:25:29 -0800

    Remove frontend from distro packaging

--- package-build.sh
+++ package-build.sh
@@ -8,21 +8,16 @@ LATEST_BUILD_DIR=`ls -t ${INTEGRATION_SHARED_DIR}/|head -1`
 DISTRO_DIR=home/tomcat7/.OpenMRS/distribution
 MODULE_DIR=home/tomcat7/.OpenMRS/modules
 OWA_DIR=home/tomcat7/.OpenMRS/owa
-FRONTEND_DIR=home/tomcat7/.OpenMRS/frontend
-SITE_FRONTEND_DIR_RELATIVE_TO_FRONTEND=../configuration/frontend
 
 rm -fR ${DISTRO_DIR} && mkdir ${DISTRO_DIR}
 rm -fR ${MODULE_DIR} && mkdir ${MODULE_DIR}
 rm -fR ${OWA_DIR} && mkdir ${OWA_DIR}
-rm -fR ${FRONTEND_DIR} && mkdir ${FRONTEND_DIR}
 
 unzip ${INTEGRATION_SHARED_DIR}/${LATEST_BUILD_DIR}/distribution-zip/pihemr*.zip -d  ${DISTRO_DIR}/
 mv ${DISTRO_DIR}/pihemr-*/openmrs_webapps/openmrs.war ./openmrs.war
 mv ${DISTRO_DIR}/pihemr-*/openmrs_modules/* ${MODULE_DIR}/
 mv ${DISTRO_DIR}/pihemr-*/openmrs_owas/* ${OWA_DIR}/
 rename 's/\.owa$/.zip/' ${OWA_DIR}/*.owa
-mv ${DISTRO_DIR}/pihemr-*/openmrs_frontend/* ${FRONTEND_DIR}/
-ln -s ${SITE_FRONTEND_DIR_RELATIVE_TO_FRONTEND} ${FRONTEND_DIR}/site
 
 rm -fR ${DISTRO_DIR}
 
